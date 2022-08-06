27d60fedf096b28547f4e25be8477c2847906c10
Geuni <dlrmsgnl620@naver.com>
2022-08-07 00:15:11 +0900

    Add: persist test

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,19 @@ import App from "./App";
 import {store} from "./redux/store";
 import {Provider} from "react-redux";
 
+import {PersistGate} from "redux-persist/integration/react";
+import {persistStore} from "redux-persist";
+
 const container = document.getElementById("root");
 const root = createRoot(container as Element);
+let persistor = persistStore(store);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,26 @@
 import {configureStore} from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {persistReducer} from "redux-persist";
+import {combineReducers} from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
 import counterReducer from "./counterSlice";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const reducers = combineReducers({
+  counter: counterReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
