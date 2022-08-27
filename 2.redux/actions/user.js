28c98e246000f60be2e5a0925921473d90553b0f
Geuni <dlrmsgnl620@naver.com>
2022-08-28 01:24:11 +0900

    Add: react-redux: 2-4. redux-thunk

--- 2.redux/actions/user.js
+++ 2.redux/actions/user.js
@@ -1,7 +1,36 @@
 const login = (data) => {
+  // async action creator
+  return (dispatch, getState) => {
+    // async action
+    dispatch(loginRequest(data));
+    try {
+      setTimeout(() => {
+        dispatch(loginSuccess({userId: 1, nickName: "keunhwee"}));
+      }, 2000);
+    } catch (err) {
+      dispatch(loginFailure(e));
+    }
+  };
+};
+
+const loginRequest = (data) => {
   return {
-    type: "LOG_IN",
+    type: "LOG_IN_REQUEST",
     data,
+  };
+};
+
+const loginSuccess = (data) => {
+  return {
+    type: "LOG_IN_SUCCESS",
+    data,
+  };
+};
+
+const loginFailure = (error) => {
+  return {
+    type: "LOG_IN_FAILURE",
+    error,
   };
 };
 
--- 2.redux/index.js
+++ 2.redux/index.js
@@ -1,4 +1,4 @@
-const {createStore} = require("redux");
+const {createStore, applyMiddleware, compose} = require("redux");
 const {login, logout} = require("./actions/user");
 const {addPost} = require("./actions/post");
 const reducer = require("./reducers");
@@ -11,7 +11,21 @@ const initialState = {
   posts: [],
 };
 
-const store = createStore(reducer, initialState);
+const firstMiddleware = (store) => (dispatch) => (action) => {
+  console.log("로깅", action);
+  dispatch(action);
+};
+
+const thunkMiddleware = (store) => (dispatch) => (action) => {
+  if (typeof action === "function") {
+    // 비동기
+    return action(store.dispatch, store.getState());
+  }
+  return dispatch(action);
+};
+const enhancer = applyMiddleware(firstMiddleware, thunkMiddleware);
+
+const store = createStore(reducer, initialState, enhancer);
 
 store.dispatch(
   login({
@@ -21,12 +35,12 @@ store.dispatch(
   })
 );
 
-store.dispatch(
-  addPost({
-    content: "안녕하세요.",
-  })
-);
-
-store.dispatch(logout());
-
 console.log("2nd", store.getState());
+
+// store.dispatch(
+//   addPost({
+//     content: "안녕하세요.",
+//   })
+// );
+
+// store.dispatch(logout());
