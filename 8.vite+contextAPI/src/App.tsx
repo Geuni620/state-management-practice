685e4194b3775d1a0b1b82c2ddd1e9106b74969b
Geuni <dlrmsgnl620@naver.com>
2022-09-08 01:57:06 +0900

    Modify: contextAPI, TODO 완료할 것

--- 8.vite+contextAPI/src/App.tsx
+++ 8.vite+contextAPI/src/App.tsx
@@ -1,34 +1,63 @@
-import { createContext, useContext } from 'react';
-import { useMyContext } from './hooks/useMyContext';
+import {
+  createContext,
+  useState,
+  useContext,
+  Dispatch,
+  SetStateAction,
+} from 'react';
 
-function App() {
-  return <AwesomeComponent value="Hello" />;
+type Props = {
+  children: React.ReactNode;
+};
+
+const CounterContext = createContext(0);
+
+function CounterProvider({ children }: Props) {
+  const [counterState, setCounterState] = useState(1);
+  return (
+    <CounterContext.Provider value={counterState}>
+      {children}
+    </CounterContext.Provider>
+  );
 }
 
-export default App;
+function useCounterState() {
+  const value = useContext(CounterContext);
+  if (value === undefined) {
+    throw new Error('useCounterState should be used within CounterProvider');
+  }
+  return value;
+}
 
-type valueProps = { value: string };
+const App = () => {
+  return (
+    <CounterProvider>
+      <div>
+        <Value />
+        <Buttons />
+      </div>
+    </CounterProvider>
+  );
+};
 
-function AwesomeComponent({ value }: valueProps) {
+// TODO: [Symbol.iterator]()' 메서드가 있어야 합니다. 해결할 것
+function Value() {
+  const [counter] = useCounterState();
+  return <h1>{counter}</h1>;
+}
+
+// TODO: [Symbol.iterator]()' 메서드가 있어야 합니다. 해결할 것
+function Buttons() {
+  const [, setCounter] = useCounterState();
+  const increase = () => setCounter((prev: number) => prev + 1);
+  const decrease = () => setCounter((prev: number) => prev - 1);
+
   return (
     <div>
-      {value}
-      <FirstComponent />
-      <SecondComponent />
-      <ThirdComponent />
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
 
-function FirstComponent() {
-  return <div>First</div>;
-}
-
-function SecondComponent() {
-  return <div>Second</div>;
-}
-
-function ThirdComponent() {
-  const value = useMyContext();
-  return <div>{value}</div>;
-}
+export default App;
